eafc99600ed41e03fc7600b4a40a618db35c7fac
Max Zaytsev <max.zaytsev@team.bumble.com>
2026-01-13 10:48:07 +0000

    Add the Paywall route

---
+++ app/paywall/page.tsx
@@ -0,0 +1,5 @@
+import { Paywall } from "@/components/Paywall/Paywall";
+
+export default function PaywallPage() {
+  return <Paywall />;
+}
---
+++ components/Paywall/Paywall.tsx
@@ -0,0 +1,8 @@
+export function Paywall() {
+  return (
+    <div>
+      <h1>Paywall</h1>
+      <p>Subscribe to access premium content</p>
+    </div>
+  );
+}
